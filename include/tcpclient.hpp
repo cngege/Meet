0add32733b2d4784517c7b064e9331d8ce5f9b40
muzili <2586850402@qq.com>
2022-05-11 21:01:05 +0800

    构建线程接收网络信息部分

--- include/tcpclient.hpp
+++ include/tcpclient.hpp
@@ -17,6 +17,7 @@
 #define ___MIRACLEFOREST_MEET_TCPCLIENT___
 
 #include <string>
+#include <thread>
 #include "ip.hpp"
 
 namespace meet
@@ -43,8 +44,11 @@ namespace meet
 			if (sockfd)
 			{
 				closesocket(sockfd);
+				connected = false;
 			}
 		}
+	public:
+		using DisConnectEvent = void(__fastcall*)();
 
 	private:
 		SOCKET sockfd;
@@ -52,21 +56,13 @@ namespace meet
 		bool connected = false;
 		Family family;
 
-	public:
+		//thread
+		std::thread recv_thread;
 
-		/// <summary>
-		/// 初始化客户端  初始化前先注册好各种监听函数
-		/// </summary>
-		/// <returns></returns>
-		auto Init() -> bool
-		{
-			if ((sockfd = socket(sock.sin_family, SOCK_STREAM, 0)) == -1)
-			{
-				return false;
-			}
-			// 开启一个线程 循环接收网络消息
-			return true;
-		};
+		//Event
+		DisConnectEvent disConnectEvent;
+
+	public:
 
 		/// <summary>
 		/// 连接远程主机
@@ -76,17 +72,29 @@ namespace meet
 		/// <returns>连接成功/失败</returns>
 		auto Connect(IP ip, unsigned short port) -> bool
 		{
+			if (connected)
+			{
+				return true;
+			}
+			if ((sockfd = socket(sock.sin_family, SOCK_STREAM, 0)) == -1)
+			{
+				return false;
+			}
 			sock.sin_port = htons(port);
 			memcpy(&sock.sin_addr, ip.GetHost()->h_addr, ip.GetHost()->h_length);
 			if (connect(sockfd, (struct sockaddr*)&sock, sizeof(sock)) != 0)
 			{
-				//perror("connect");
 				closesocket(sockfd);
 				return false;
 			}
 			connected = true;
+			
+			// 开启线程 接收并处理消息后触发监听事件
+			recv_thread = std::thread(startRecv,this);
+			recv_thread.detach();
 			return true;
 		};
+
 
 		//断开连接 调用函数断开与主机的连接
 		auto DisConnect() -> bool
@@ -104,11 +112,46 @@ namespace meet
 		{
 
 		};
+	public:
+		// 注册回调事件
+		void OnDisConnect(DisConnectEvent _disConnectEvent)
+		{
+			disConnectEvent = _disConnectEvent;
+		};
 
 	private:
-		auto StartRecv() -> void
+		static auto startRecv(TCPClient* c) -> void
 		{
+			if (!c->connected)
+			{
+				char buffer[5];
+				memset(buffer, 0, sizeof(buffer));
 
+				while (c->connected)
+				{
+					int recvbytecount;
+					if ((recvbytecount = recv(c->sockfd, buffer, sizeof(buffer), 0)) <= 0)
+					{
+						//返回 0 网络中断
+						if (recvbytecount == 0)
+						{
+							c->connected = false;
+							c->disConnectEvent();
+							break;
+						}
+						if (recvbytecount == SOCKET_ERROR)
+						{
+							//copy数据时出错
+							//调用错误回调
+						}
+					}
+
+					if (recvbytecount < 5)
+					{
+						continue;
+					}
+				}
+			}
 		};
 	};
 
