6d7c0ba269b85455153b3eb51512a5d758f5b205
muzili <2586850402@qq.com>
2024-10-27 03:22:04 +0800

    尝试适配CMAKE自动引用

---
+++ MeetConfig.cmake
@@ -0,0 +1,14 @@
+﻿if(NOT DEFINED Meet_NAME)
+    set(Meet_NAME "Meet")
+endif()
+
+
+# Meet::Meet is header-only:
+add_library(${Meet_NAME} INTERFACE)
+
+target_include_directories(${Meet_NAME} INTERFACE
+    "${CMAKE_CURRENT_LIST_DIR}/include/"
+)
+
+add_library("${Meet_NAME}::Meet" ALIAS ${Meet_NAME})
+
